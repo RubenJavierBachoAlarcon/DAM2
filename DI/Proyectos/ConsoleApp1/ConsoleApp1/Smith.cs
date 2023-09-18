5b7f2cdc33a66a53d7d357d6dd30bff41a47181f
Ruben Javier Bacho Alarcon <rjbachoa01@educastillalamancha.es>
2023-09-18 14:28:31 +0100

    Matrix2

---
+++ DI/Proyectos/ConsoleApp1/ConsoleApp1/Smith.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Smith
+{
+	private int capInfect;
+
+    public Smith(int capInfect)
+    {
+        this.capInfect = this.capInfect;
+    }
+
+    public Smith() : this(5)
+    {
+    }
+}
