8588c597d0d5f7111a238913c95f69e59cdf0ab7
rjbachoa01 <rjbachoa01@educastillalamancha.es>
2023-09-27 10:47:14 +0200

    AndroidStudio

--- PMDM/TipTime2/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ PMDM/TipTime2/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,8 +1,10 @@
+package com.example.tiptime
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.RadioButton
 import android.widget.TextView
-import com.example.tiptime.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,24 +15,28 @@ class MainActivity : AppCompatActivity() {
         val checkBox1 = findViewById<RadioButton>(R.id.option_twenty_percent)
         val checkBox2 = findViewById<RadioButton>(R.id.option_twenty_percent2)
         val checkBox3 = findViewById<RadioButton>(R.id.option_twenty_percent3)
+        val buttonCalculate = findViewById<Button>(R.id.buttonCalculate)
         var solutionNum = 0.0  // Use a Double to store decimal values
 
         // Convert the text to a Double if it's a valid number
-        val costText = costService.text.toString()
-        val costValue = costText.toDoubleOrNull()
 
-        if (costValue != null) {
-            if (checkBox1.isChecked) {
-                solutionNum = costValue * 0.2
-            } else if (checkBox2.isChecked) {
-                solutionNum = costValue * 0.18
-            } else if (checkBox3.isChecked) {
-                solutionNum = costValue * 0.15
+        buttonCalculate.setOnClickListener(){
+            val costText = costService.text.toString()
+            val costValue = costText.toDoubleOrNull()
+            if (costValue != null) {
+                if (checkBox1.isChecked) {
+                    solutionNum = costValue * 0.2
+                } else if (checkBox2.isChecked) {
+                    solutionNum = costValue * 0.18
+                } else if (checkBox3.isChecked) {
+                    solutionNum = costValue * 0.15
+                }
+            } else {
+
             }
-        } else {
-            // Handle the case where the input is not a valid number
-            // You might want to show an error message or take appropriate action
+            solution.text = solutionNum.toString()
         }
+
 
         // Convert solutionNum to a String before setting it as the text
         solution.text = solutionNum.toString()
