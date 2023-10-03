7501c28c02c9d9df5423a0b55aac8071541351f4
Ruben Javier Bacho Alarcon <rjbachoa01@educastillalamancha.es>
2023-10-03 11:27:33 +0100

    VSProyectos

---
+++ DI/Proyectos/17Piramid/Ejercicio16Piramid/Program.cs
@@ -0,0 +1,18 @@
+﻿int length = 5; // Change this to change the size of the pyramid
+
+for (int i = 1; i <= length; i++)
+{
+    // Spaces before the asterisks
+    for (int j = 1; j <= length - i; j++)
+    {
+        Console.Write("  ");
+    }
+
+    // Asterisks
+    for (int j = 1; j <= 2 * i - 1; j++)
+    {
+        Console.Write("* ");
+    }
+
+    Console.WriteLine(); // Move to the next line
+}
