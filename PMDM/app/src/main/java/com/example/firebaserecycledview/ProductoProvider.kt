796f44cd5d1157341ddc5eee96714764573a34ae
Ruben Javier Bacho Alarcon <130557846+RubenJavierBachoAlarcon@users.noreply.github.com>
2023-10-30 10:36:04 +0100

    Log.d

--- PMDM/app/src/main/java/com/example/firebaserecycledview/ProductoProvider.kt
+++ PMDM/app/src/main/java/com/example/firebaserecycledview/ProductoProvider.kt
@@ -1,13 +1,14 @@
 package com.example.firebaserecycledview
 
+import android.util.Log
 import com.example.firebaserecycledview.adapter.Producto
 import com.google.firebase.firestore.QuerySnapshot
 
 class ProductoProvider {
     var mutableList = mutableListOf<Producto>()
 
-    fun actualizarLista(resultado: QuerySnapshot){
-        for (document in resultado){
+    fun actualizarLista(resultado: QuerySnapshot) {
+        for (document in resultado) {
             val producto = Producto(
                 document.id.toInt(),
                 document.data["nombre"].toString(),
@@ -15,7 +16,10 @@ class ProductoProvider {
                 document.data["foto"].toString()
             )
             mutableList.add(producto)
+            // Agrega un mensaje de depuración para verificar
+            Log.d("Firestore", "Producto agregado: $producto")
         }
     }
+
 
 }
