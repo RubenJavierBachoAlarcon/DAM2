c010b21397392d4506e7a303b737397bb0546479
Ruben Javier Bacho Alarcon <rjbachoa01@educastillalamancha.es>
2023-11-14 06:59:35 +0100

    jeje x2

--- SGE/EjerciciosPython Todos/Ejercicio copy.py
+++ SGE/EjerciciosPython Todos/Ejercicio copy.py
@@ -6,6 +6,9 @@ print(cadenaTrim)
 print(cadenaReverse)
 
 if (cadenaTrim == cadenaReverse):
-    print("palindromo")
+    print(f"palindromo {cadenaTrim} y {cadenaReverse}")
 else:
     print("No es palindromo")
+
+n=4
+print(f"{'Número':<8}{'Cuadrado':<8}{'Cubo':<8}")
