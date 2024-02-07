97dd169ac746eaa6da67b835ce095c13a9f26fd1
Ruben Javier Bacho Alarcon <rjbachoa01@educastillalamancha.es>
2024-02-07 19:02:36 +0100

    crystal report completo

---
+++ PROYECTO COMUNIDAD/Proyecto Comunidad/Proyecto Comunidad/CommunityInfo.xaml.cs
@@ -0,0 +1,51 @@
+﻿using DI_Ejercicio_4_Squid_Game.Persistance.DataBaseFolder;
+using SAPBusinessObjects.WPF.Viewer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_Comunidad
+{
+    /// <summary>
+    /// Interaction logic for CommunityInfo.xaml
+    /// </summary>
+    public partial class CommunityInfo : Window
+    {
+        public CommunityInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void CrystalReportsViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<List<Object>> owners = DataBase.obtainDataBase().read("SELECT id, name FROM owners");
+
+            DataSet1 ds = new DataSet1();
+
+            // Llena la tabla owners de tu DataSet con los datos que obtuviste
+            foreach (List<Object> row in owners)
+            {
+                ds.owners.Rows.Add(row.ToArray());
+            }
+
+            CrystalReport1 report = new CrystalReport1();
+
+            // Asigna el DataSet al informe
+            report.SetDataSource(ds);
+
+            // Asigna el informe al visor de informes de Crystal
+            crystalReportsViewer.ViewerCore.ReportSource = report;
+
+        }
+    }
+}
--- PROYECTO COMUNIDAD/Proyecto Comunidad/Proyecto Comunidad/Page4.xaml.cs
+++ PROYECTO COMUNIDAD/Proyecto Comunidad/Proyecto Comunidad/Page4.xaml.cs
@@ -38,7 +38,7 @@ namespace Proyecto_Comunidad
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void NEXT_Click(object sender, RoutedEventArgs e)
         {
             isGateKeeper = GATE.IsChecked.Value;
 
@@ -70,13 +70,13 @@ namespace Proyecto_Comunidad
 
             DataBase.obtainDataBase().modify("INSERT INTO community_info VALUES " +
                 $"('{GlobalVariable.nameCommunity}', {isShowers}, {isPlayground}, {isGym}, {isMeetingRoom}, {isTennisCourt}, {isPadelCourt})");
- 
+
 
 
 
             if (isGateKeeper)
             {
-             
+
 
 
                 List<List<Object>> gateKeeper = DataBase.obtainDataBase().read("SELECT * FROM owners WHERE id = -1");
@@ -101,13 +101,40 @@ namespace Proyecto_Comunidad
             }
 
 
-            MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
-            Page2 page2 = new Page2();
-            mainWindow.MainFrame.Navigate(page2);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("INSERT INTO owners (id, DNI, Name, Surname, Address, Locality, zipCode, province) VALUES ");
+
+            string[] names = { "John", "Jane", "Bob", "Alice", "Mike", "Emily", "David", "Samantha", "Tom", "Linda" };
+            string[] surnames = { "Smith", "Johnson", "Williams", "Jones", "Brown", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez" };
+            Random rnd = new Random();
+
+            for (int i = 1; i < 200; i++)
+            {
+                sb.Append($"({i},'{i + 123}', '{names[rnd.Next(names.Length)]}', '{surnames[rnd.Next(surnames.Length)]}', 'Address{i}', 'Locality{i}', '{i}', 'Province{i}')");
+
+                if (i != 199)
+                {
+                    sb.Append(",");
+                }
+            }
+
+            DataBase.obtainDataBase().modify(sb.ToString());
+
+            DataBase.obtainDataBase().modify("INSERT INTO entrances_owners (idEntrance, idOwner)\r\nSELECT entrances.id, owners.id\r\nFROM entrances\r\nCROSS JOIN owners\r\nORDER BY RAND()\r\nLIMIT 200;\r\n");
 
 
+            CommunityInfo communityInfoWindow = new CommunityInfo();
+
+            communityInfoWindow.Show();
 
 
+            Window currentWindow = Window.GetWindow(this);
+
+            // Cierra la ventana actual
+            if (currentWindow != null)
+            {
+                currentWindow.Close();
+            }
 
         }
     }
